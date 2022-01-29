eaff06beededee93532f8b1dcf2bad70197cd850
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-29 21:51:51 +0100

    Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,8 +65,8 @@ tasks {
     }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     jar {
         enabled = false
