c0e27908e12b231fc08bc6091ac4c232bc9dcdc8
Alexander Staeding <alexanderstaeding@gmail.com>
2022-02-06 13:17:08 +0100

    Fix h08 -> h12

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ tasks {
         group = "build"
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         val runtimeDeps = grader.runtimeClasspath.mapNotNull {
-            if (it.path.toLowerCase().contains("h08")) {
+            if (it.path.toLowerCase().contains("h12")) {
                 null
             } else if (it.isDirectory) {
                 it
@@ -60,7 +60,7 @@ tasks {
             }
         }
         from(runtimeDeps)
-        archiveFileName.set("FOP-2022-H08-${project.version}-libs.jar")
+        archiveFileName.set("FOP-2022-H12-${project.version}-libs.jar")
     }
     create("graderAll") {
         group = "build"
