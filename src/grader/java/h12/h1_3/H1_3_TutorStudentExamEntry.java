103bdd81533d76ab015f009fc6f1e52ae13e4032
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-11 10:46:26 +0100

    Override equals, hashcode and toString in H2_2 and H1_3 StudentExamEntry tutor implementations

--- src/grader/java/h12/h1_3/H1_3_TutorStudentExamEntry.java
+++ src/grader/java/h12/h1_3/H1_3_TutorStudentExamEntry.java
@@ -3,6 +3,7 @@ package h12.h1_3;
 import h12.StudentExamEntry;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class H1_3_TutorStudentExamEntry extends StudentExamEntry {
@@ -22,5 +23,31 @@ public class H1_3_TutorStudentExamEntry extends StudentExamEntry {
         if (SET_MARK_CONSUMER == null || SET_MARK_CONSUMER.test(mark)) {
             super.setMark(mark);
         }
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof final StudentExamEntry that)) return false;
+        return getEnrollmentNumber() == that.getEnrollmentNumber()
+            && Objects.equals(getFirstName(), that.getFirstName())
+            && Objects.equals(getLastName(), that.getLastName())
+            && Objects.equals(getMark(), that.getMark());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getFirstName(), getLastName(), getEnrollmentNumber(), getMark());
+    }
+
+    @Override
+    public String toString() {
+        return "H1_3_TutorStudentExamEntry{" +
+            "firstName='" + getFirstName() + '\'' +
+            ", lastName='" + getLastName() + '\'' +
+            ", enrollmentNumber=" + getEnrollmentNumber() +
+            ", mark='" + getMark() + '\'' +
+            '}';
     }
 }
--- src/grader/java/h12/h2_2and3/H2_2_TutorStudentExamEntry.java
+++ src/grader/java/h12/h2_2and3/H2_2_TutorStudentExamEntry.java
@@ -2,6 +2,8 @@ package h12.h2_2and3;
 
 import h12.StudentExamEntry;
 
+import java.util.Objects;
+
 public class H2_2_TutorStudentExamEntry extends StudentExamEntry {
 
     public static boolean INVOKE_3;
@@ -20,5 +22,30 @@ public class H2_2_TutorStudentExamEntry extends StudentExamEntry {
     public H2_2_TutorStudentExamEntry(final String firstName, final String lastName, final int enrollmentNumber, final String mark) {
         super(firstName, lastName, enrollmentNumber, mark);
         INVOKE_4 = true;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof final StudentExamEntry that)) return false;
+        return getEnrollmentNumber() == that.getEnrollmentNumber()
+            && Objects.equals(getFirstName(), that.getFirstName())
+            && Objects.equals(getLastName(), that.getLastName())
+            && Objects.equals(getMark(), that.getMark());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getFirstName(), getLastName(), getEnrollmentNumber(), getMark());
+    }
+
+    @Override
+    public String toString() {
+        return "H2_2_TutorStudentExamEntry{" +
+            "firstName='" + getFirstName() + '\'' +
+            ", lastName='" + getLastName() + '\'' +
+            ", enrollmentNumber=" + getEnrollmentNumber() +
+            ", mark='" + getMark() + '\'' +
+            '}';
     }
 }
